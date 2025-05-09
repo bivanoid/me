f12bb0d7d5bafc5a273ec853ec77010caf80962f
Vandya <143333233+Vandyaaa@users.noreply.github.com>
2025-05-09 10:22:34 +0700

    update 5

--- src/components/horizontalslider.js
+++ src/components/horizontalslider.js
@@ -11,7 +11,25 @@ import '../styles/scrollhorizontal.css';
 // import required modules
 import { Pagination } from 'swiper/modules';
 
+  
+
+
 export default function HorizontalSlider() {
+  const [fullscreenImage, setFullscreenImage] = useState(null);
+
+  const handleImageClick = (e) => {
+  const slide = e.currentTarget;
+  const bgImage = window.getComputedStyle(slide).backgroundImage;
+  const urlMatch = bgImage.match(/url\("(.*)"\)/);
+  if (urlMatch) {
+    setFullscreenImage(urlMatch[1]);
+  }
+};
+
+
+  const closeFullscreen = () => {
+    setFullscreenImage(null);
+  };
   return (
     <div className='con-swiper'>
       <div className='title-swiper'><h1>What recent i do</h1> <p>slide</p></div>
@@ -29,23 +47,30 @@ export default function HorizontalSlider() {
   modules={[Pagination]}
   className="mySwiper"
 >
-        <SwiperSlide className='gambar gambar1'>
+        <SwiperSlide onClick={handleImageClick} className='gambar gambar1'>
           <h1>Bicture Apps (Gallery Photo Online)</h1>
         </SwiperSlide>
-        <SwiperSlide className='gambar gambar2'>
-          <h1>Title 1</h1>
+        <SwiperSlide onClick={handleImageClick} className='gambar gambar2'>
+          <h1>Al-Quran Website</h1>
         </SwiperSlide>
-        <SwiperSlide className='gambar gambar3'>
-          <h1>Title 1</h1>
+        <SwiperSlide onClick={handleImageClick} className='gambar gambar3'>
+          <h1>Administration</h1>
         </SwiperSlide>
-        <SwiperSlide className='gambar gambar4'>
-          <h1>Title 1</h1>
+        <SwiperSlide onClick={handleImageClick} className='gambar gambar4'>
+          <h1>To do List</h1>
         </SwiperSlide>
 
       </Swiper>
       <div className='con-all-project'>
         <div className='circle-arrow-all-project'><i class="fi fi-rs-arrow-right"></i></div><p>See All</p>
       </div>
+      {fullscreenImage && (
+        <div className="fullscreen-overlay" onClick={closeFullscreen}>
+          <img src={fullscreenImage} alt="Fullscreen" />
+        </div>
+      )}
     </div>
   );
+
+
 }
