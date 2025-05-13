0e2642003400ce751504c8e8daf4362af69cbe47
Vandya <143333233+Vandyaaa@users.noreply.github.com>
2025-05-13 14:51:45 +0700

    browsrut

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Introduction from './components/introduction';
 import Navigation from './components/navigation';
